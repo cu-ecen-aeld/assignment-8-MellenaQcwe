a9d2c271be30de3895dae8eaae1f0d792f529fdd
MellenaQcwe <mellena.nziekou@gmail.com>
2024-12-10 16:33:27 +0100

    Done assignment 5 part 2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,11 +5,11 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 1.0
+AESD_ASSIGNMENTS_VERSION = 
 AESD_ASSIGNMENTS_SITE = $(AESD_ASSIGNMENTS_OVERRIDEN_SRCDIR)
 AESD_ASSIGNMENTS_SITE_METHOD = local
 ifeq ($(AESD_ASSIGNMENTS_OVERRIDEN_SRCDIR),)
-	AESD_ASSIGNMENTS_VERSION = 348baa271d5d0812dd41ca7db64d93ec607ca90d
+	AESD_ASSIGNMENTS_VERSION = 66f200c6144d569ef148224a3f02e227fc3f848e
 	AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-MellenaQcwe.git
 	AESD_ASSIGNMENTS_SITE_METHOD = git
 	AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
@@ -17,6 +17,7 @@ endif
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/server all
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
@@ -27,6 +28,9 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer ${TARGET_DIR}/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/autorun-qemu.sh ${TARGET_DIR}/usr/bin
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
